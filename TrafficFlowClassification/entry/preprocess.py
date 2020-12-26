33480b3a815e1dd9ee90db7f68e122ca12827b6d
wangmaonan <18811371255@163.com>
2020-12-26 13:06:59 +0800

    add pacp transfer and update pcap2session

--- TrafficFlowClassification/entry/preprocess.py
+++ TrafficFlowClassification/entry/preprocess.py
@@ -2,13 +2,18 @@
 @Author: WANG Maonan
 @Date: 2020-12-15 16:53:21
 @Description: 对原始流量文件进行预处理
-@LastEditTime: 2020-12-16 13:16:44
+@LastEditTime: 2020-12-25 19:12:38
 '''
 import os
 import yaml
+import shutil
+from easydict import EasyDict
 
 from TrafficFlowClassification.TrafficLog.setLog import logger
+
+
 from TrafficFlowClassification.preprocess.pcapng2pcap import pcapng_to_pcap
+from TrafficFlowClassification.preprocess.pcapTransfer import pcap_transfer
 from TrafficFlowClassification.preprocess.pcap2session import pcap_to_session
 
 def setup_config():
@@ -16,16 +21,42 @@ def setup_config():
     """
     with open(os.path.join(os.path.dirname(__file__), './preprocess.yaml'), encoding='utf8') as f:
         cfg = yaml.safe_load(f)
+    cfg = EasyDict(cfg)
     return cfg
+
+def transfer_pcap(before_path, after_path):
+    """将 before_path 中的所以文件转移到 after_path 中去
+
+    Args:
+        before_path (str): 转移之前的文件路径
+        after_path (str): 转移之后的文件路径
+    """
+    ignore_list = ['youtubeHTML5_1.pcap', 'torFacebook.pcap', 'torGoogle.pcap', 'torTwitter.pcap'] # 不需要转移的 pcap 文件
+    os.makedirs(after_path, exist_ok=True) # 新建目标目录
+    for file in os.listdir(before_path):
+        src_path = os.path.join(before_path, file)
+        dst_path = os.path.join(after_path, file)
+        if file in ignore_list:
+            logger.info('忽略文件 {}'.format(src_path))
+        else:
+            logger.info('开始转移文件, {} --> {}'.format(src_path, dst_path))
+            shutil.copy(src_path, dst_path)
+    logger.info('文件全部转移完成')
+    logger.info('=============\n')
 
 def preprocess_pipeline():
     """对流量进行预处理, 处理流程为:
-    0. 首先将不同的流量新建文件夹, 分成不同的类别
-    1. 首先将 pcapng 文件转换为 pcap 文件
-    2. 接着将 pcap 文件按照五元组分为不同的 session, 使用 SplitCap 来完成
+    0. 将所有流量文件转移到新的文件夹
+    1. 接着将 pcapng 文件转换为 pcap 文件
+    2. 接着将不同的流量新建文件夹, 分成不同的类别, pcap transfer
+    3. 接着将 pcap 文件按照五元组分为不同的 session, 使用 SplitCap 来完成
+    4. 对 session 进行处理, 匿名化, 删除应用层是空的 session
+    5. 对于每一类的文件, 划分训练集和测试集
     """
     cfg = setup_config() # 获取 config 文件
     logger.info(cfg)
 
-    # pcapng_to_pcap(cfg['pcap_path']['raw_pcap_path']) # 将 pcapng 转换为 pcap
-    pcap_to_session(cfg['pcap_path']['raw_pcap_path'], cfg['tool_path']['splitcap_path']) # 将 pcap 转换为 session
+    transfer_pcap(cfg.pcap_path.raw_pcap_path, cfg.pcap_path.new_pcap_path) # 转移文件
+    pcapng_to_pcap(cfg.pcap_path.new_pcap_path) # 将 pcapng 转换为 pcap
+    pcap_transfer(cfg.pcap_path.new_pcap_path, cfg.pcap_path.new_pcap_path) # 将文件放在指定文件夹中
+    pcap_to_session(cfg.pcap_path.new_pcap_path, cfg.tool_path.splitcap_path) # 将 pcap 转换为 session
