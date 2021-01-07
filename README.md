bd3dd3a91c07bda9154982db21370e1e2b8b09de
wangmaonan <18811371255@163.com>
2021-01-07 18:06:33 +0800

    add train process

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  * @Author: WANG Maonan
  * @Date: 2020-12-15 19:40:58
  * @Description: 使用说明
- * @LastEditTime: 2020-12-15 19:41:30
+ * @LastEditTime: 2021-01-07 14:50:52
 -->
 
 在根目录下运行下面命令, 执行数据预处理
@@ -10,3 +10,8 @@
 ```python
 python -m TrafficFlowClassification preprocess_pipeline
 ```
+
+## 待做事项
+
+- [ ] 分析一下 pcap 包含字节的数量
+- [ ] 分析一下每一类文件包含的数量
---
+++ TrafficFlowClassification/train/trainProcess.py
@@ -0,0 +1,49 @@
+'''
+@Author: WANG Maonan
+@Date: 2021-01-07 17:03:15
+@Description: 模型训练的流程, 这里是一个 epoch 的训练流程
+@LastEditTime: 2021-01-07 18:03:53
+'''
+
+from TrafficFlowClassification.utils.helper import AverageMeter, accuracy
+from TrafficFlowClassification.TrafficLog.setLog import logger
+
+
+def train_process(train_loader, model, criterion, optimizer, epoch, device, print_freq):
+    """训练一个 epoch 的流程
+
+    Args:
+        train_loader (dataloader): [description]
+        model ([type]): [description]
+        criterion ([type]): [description]
+        optimizer ([type]): [description]
+        epoch (int): 当前所在的 epoch
+        device (torch.device): 是否使用 gpu
+        print_freq ([type]): [description]
+    """
+    losses = AverageMeter()  # 在一个 train loader 中的 loss 变化
+    top1 = AverageMeter()  # 记录在一个 train loader 中的 accuracy 变化
+
+    model.train()  # 切换为训练模型
+
+    for i, (input, target) in enumerate(train_loader):
+
+        target = target.to(device)
+        input = input.to(device)
+
+        output = model(input)  # 得到模型预测结果
+        loss = criterion(output, target)  # 计算 loss
+
+        # 计算准确率, 记录 loss 和 accuracy
+        prec1 = accuracy(output.data, target)
+        losses.update(loss.item(), input.size(0))
+        top1.update(prec1[0], input.size(0))
+
+        # 反向传播
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if i % print_freq == 0:
+            logger('Epoch: [{0}][{1}/{2}], Loss {loss.val:.4f} ({loss.avg:.4f}), Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'.format(
+                epoch, i, len(train_loader), loss=losses, top1=top1))
